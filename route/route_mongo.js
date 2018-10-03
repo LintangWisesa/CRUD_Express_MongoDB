ade48922368d7270fdc8acede0cbd939bf65ca41
lintangwisesa <lintangwisesa@ymail.com>
2018-10-03 11:14:44 +0700

    express mongo

--- route/route_mongo.js
+++ route/route_mongo.js
@@ -23,8 +23,58 @@ router.get('/data/:index', (req, res)=>{
     mongoClient.connect(url, (error, db)=>{
         var col = db.collection('datamongo')
         col.find({}).toArray((er, data)=>{
-            console.log(data[req.params.index-1])
-            res.send(data[req.params.index-1])
+            console.log(data[req.params.index - 1])
+            res.send(data[req.params.index - 1])
+        })
+    })
+})
+
+// route post data ke database
+router.post('/data', (req, res)=>{
+    mongoClient.connect(url, (error, db)=>{
+        var col = db.collection('datamongo')
+        col.insertMany([{
+            nama: req.body.nama,
+            usia: req.body.usia, 
+            kota: req.body.kota
+        }], (error, hasil)=>{
+            console.log(hasil)
+            res.send(hasil)
+        })
+    })
+})
+
+// edit data tertentu
+router.put('/data/:id', (req, res)=>{
+    mongoClient.connect(url, (error, db)=>{
+        var col = db.collection('datamongo')
+        col.updateOne(
+            {_id: new mongodb.ObjectID(
+                `${req.params.id}`
+            )}, 
+            {$set: {
+                nama: req.body.nama,
+                usia: req.body.usia, 
+                kota: req.body.kota
+            }},
+            (error, hasil)=>{
+                console.log(hasil)
+                res.send(hasil)
+        })
+    })
+})
+
+// delete id tertentu
+router.delete('/data/:id', (req, res)=>{
+    mongoClient.connect(url, (error, db)=>{
+        var col = db.collection('datamongo')
+        col.deleteOne(
+            {_id: new mongodb.ObjectID(
+                `${req.params.id}`
+            )},
+            (error, hasil)=>{
+                console.log(hasil)
+                res.send(hasil)
         })
     })
 })
