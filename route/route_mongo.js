16aab32c29b996454da9a0254e9c0c6e53c004a1
lintangwisesa <lintangwisesa@ymail.com>
2018-10-03 10:28:41 +0700

    express mongo

--- route/route_mongo.js
+++ route/route_mongo.js
@@ -1,18 +1,30 @@
 var router = require('express').Router()
 var bodyParser = require('body-parser')
-var mongoCLient = require('mongodb').MongoClient
+var mongoClient = require('mongodb').MongoClient
 var mongodb = require('mongodb')
 router.use(bodyParser.json())
 
 var url = 
 'mongodb://lin:12345@localhost:27017/expmongo'
 
+// route get semua data 
 router.get('/data', (req, res)=>{
     mongoClient.connect(url, (error, db)=>{
         var col = db.collection('datamongo')
         col.find({}).toArray((er, data)=>{
             console.log(data)
             res.send(data)
+        })
+    })
+})
+
+// route untuk ambil data tertentu (by index array)
+router.get('/data/:index', (req, res)=>{
+    mongoClient.connect(url, (error, db)=>{
+        var col = db.collection('datamongo')
+        col.find({}).toArray((er, data)=>{
+            console.log(data[req.params.index-1])
+            res.send(data[req.params.index-1])
         })
     })
 })
