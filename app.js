2264a14c188f0f4cbc6d49cd4d071adfa49abff2
lintangwisesa <lintangwisesa@ymail.com>
2018-10-03 10:14:26 +0700

    express mongo

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
+var routeSaya = require('./route/route_mongo')
 
 var app = express()
 app.use(cors())
 app.use(bodyParser.json())
+app.use(routeSaya)
 
-// bikin route di sini!
+app.get('/', (req, res)=>{
+    res.send('<h1>Express ♥ MongoDB</h1>')
+})
 
 app.listen(5000, ()=>{
     console.log('Server aktif di port 5000!')
--- route/route_mongo.js
+++ route/route_mongo.js
@@ -1,6 +1,20 @@
 var router = require('express').Router()
 var bodyParser = require('body-parser')
+var mongoCLient = require('mongodb').MongoClient
+var mongodb = require('mongodb')
 router.use(bodyParser.json())
 
+var url = 
+'mongodb://lin:12345@localhost:27017/expmongo'
+
+router.get('/data', (req, res)=>{
+    mongoClient.connect(url, (error, db)=>{
+        var col = db.collection('datamongo')
+        col.find({}).toArray((er, data)=>{
+            console.log(data)
+            res.send(data)
+        })
+    })
+})
 
 module.exports = router
